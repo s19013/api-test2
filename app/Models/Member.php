df7b597297ee8f0b4ec612b51ee241a751cfbd79
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-27 13:15:06 +0900

    ::createとかのためにモデルに色々教えている

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,11 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Member extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    // 使うテーブルを教える
+    protected $table = 'members';
+    // 主キーを教える
+    protected $primaryKey = 'id';
+
+    //idの数字を自動で増やす
+    public $incrementing = true;
+
+    // タイムスタンプを使うことを教える
+    public $timestamps = true;
 
     // 必ず埋める部分
     protected $fillable = [
