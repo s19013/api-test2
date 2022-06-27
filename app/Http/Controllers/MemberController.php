5e0ca5c9ce217e50d2188dd7905438c1dadcb281
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-27 17:20:12 +0900

    エラー処理追加

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -22,46 +22,72 @@ class MemberController extends Controller
             $member->save();
         });
 
-        return response()->json($member);
+        return response()->json([$member],201);
     }
 
     public function getAllmembers()
     {
-        $member = Member::all();
-        return response()->json($member);
+        $member = Member::get()->toJson(JSON_PRETTY_PRINT);
+        return response($member,200);
     }
 
     public function getMember($id)
     {
-        # code...
-        $member = Member::find($id);
-        return response()->json($member);
+        // メンバーが存在しているか確認
+        if (Member::where('id',$id)->whereNull('deleted_at')
+        ->exists()) {
+            $member = Member::find($id)->toJson(JSON_PRETTY_PRINT);
+            return response($member,200);
+        } else {
+            return response()->json([
+                "message" => "not founde"
+            ],404);
+        }
     }
 
     public function update(Request $request)
     {
-        // 見つけて変数に代入
-        $member = Member::find($request->id);
-        // dd($request);
+        //メンバーが存在しているか確認
+        if (Member::where('id',$request->id)->whereNull('deleted_at')
+        ->exists()){
+            // 見つけて変数に代入
+            $member = Member::find($request->id);
+            // 更新
+            DB::transaction(function () use($request,$member){
+                $member->name    = $request->name;
+                $member->gender  = $request->gender;
+                $member->age     = $request->age;
+                $member->address = $request->address;
+                $member->tel     = $request->tel;
 
-        // 更新
-        DB::transaction(function () use($request,$member){
-            $member->name    = $request->name;
-            $member->gender  = $request->gender;
-            $member->age     = $request->age;
-            $member->address = $request->address;
-            $member->tel     = $request->tel;
-
-            $member->save();
-        });
-
-        return response()->json($member);
+                $member->save();
+            });
+            return response()->json([
+                "message" => "update done",
+                "data" =>$member
+            ],200);
+        } else {
+            return response()->json([
+                "message" => "not found"
+            ],404);
+        }
     }
 
     public function delete($id)
     {
-        $member = Member::find($id);
-        $member->delete();
-        return response()->json();
+        // 存在しているか確認
+        if (Member::where('id',$id)->whereNull('deleted_at')
+        ->exists()){
+            $member = Member::find($id);
+            $member->delete();
+            return response()->json([
+                "message" => "deleted"
+            ],202);
+        } else {
+            return response()->json([
+                "message" => "not found"
+            ],404);
+        }
+
     }
 }
